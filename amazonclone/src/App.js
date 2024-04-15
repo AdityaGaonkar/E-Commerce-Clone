36f0963c4812d8c2802645d67983d3aedf24f0c0
Aditya Gaonkar <gaonkaraditya49@gmail.com>
2024-04-15 13:38:07 +0530

    commit

--- amazonclone/src/App.js
+++ amazonclone/src/App.js
@@ -1,24 +1,22 @@
+import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Checkout from "./Checkout";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/*Header*/}
-        <Switch>
-          <Route path="/checkout">
-            <Header />
-            <Checkout />
-          </Route>
-          <Route path="/">
-            <Header />
-            <Home />
-          </Route>
-        </Switch>
+        {/* Header */}
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+          {/* Wrap Checkout component with Route */}
+          <Route path="/" element={<Home />} />
+          {/* Wrap Home component with Route */}
+        </Routes>
       </div>
     </Router>
   );
